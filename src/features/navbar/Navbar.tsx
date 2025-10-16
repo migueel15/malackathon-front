ccb002ccdcc04082a48297a58d6af4810fe10287
migueel15 <miguelangeldorado10@gmail.com>
2025-10-16 19:26:39 +0200

    aaa

--- src/features/navbar/Navbar.tsx
+++ src/features/navbar/Navbar.tsx
@@ -1,5 +1,9 @@
 type Props = {};
 
 export default function Navbar({ }: Props) {
-	return <div className="">Navbar</div>;
+	return (
+		<div className="w-50 bg-gray-700 flex flex-col items-center">
+			<h2>Malackathon</h2>
+		</div>
+	)
 }
