245f726f6f768560a875911a90fa0cb9c6eba94a
migueel15 <miguelangeldorado10@gmail.com>
2025-10-16 19:20:51 +0200

    aaaa

--- src/features/navbar/Navbar.tsx
+++ src/features/navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 type Props = {};
 
-export default function Navbar({}: Props) {
-  return <div>Navbar</div>;
+export default function Navbar({ }: Props) {
+	return <div className="">Navbar</div>;
 }
--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,9 +1,9 @@
 import { Outlet } from "react-router";
+import Navbar from "../features/navbar/Navbar";
 const Layout = ({ children }: { children?: React.ReactNode }) => {
 	return (
 		<div className="flex w-screen min-h-screen">
-			Layout
-
+			<Navbar />
 			{children && <div className="flex flex-1">{children}</div>}
 			<Outlet />
 		</div>
