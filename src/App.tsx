8e806883b32b7305506ab623d1c280f906638f62
Macintosh HD <macintoshhd@Macintosh-HD.local>
2025-10-16 18:44:44 +0200

    “tailwind“

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Dashboard from "./pages/Dashboard";
+import Navbar from "./features/navbar/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <div>HOLAA</div>
+      <Navbar />
+      <Dashboard />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
