1b48d7a1bd3212163e98b8726a8b0a70e1eb5687
migueel15 <miguelangeldorado10@gmail.com>
2025-10-16 18:59:35 +0200

    base layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-    <div>HOLAA</div>
-    </>
-  )
+	return (
+		<>
+			<div>HOLAA</div>
+		</>
+	)
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+// import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import Layout from './layout/Layout.tsx'
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: (
+			<Layout />
+		),
+		children: [
+			{
+				path: "",
+				// element: 
+			}
+		]
+	}
+])
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+	<StrictMode>
+		<RouterProvider router={router} />
+	</StrictMode>,
 )
